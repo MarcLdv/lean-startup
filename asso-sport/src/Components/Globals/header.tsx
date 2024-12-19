ec154ddf67277e5c5773e2fada9144fd26dd9902
Victor LE FLOCH <vilefloch@outlook.com>
2024-12-19 11:18:10 +0100

    add section hompage

--- asso-sport/src/Components/Globals/header.tsx
+++ asso-sport/src/Components/Globals/header.tsx
@@ -1,26 +1,26 @@
 import Image from "next/image";
 import Link from "next/link";
-import {HomeIcon} from "@/Components/Globals/icons";
+import logo from "@/assets/images/logo.png";
+import {HomeIcon, PersonIcon} from "@/Components/Globals/icons";
 
 const Header = () => {
 
   return (
-    <header className="fixed top-10 left-10 right-10 bg-white rounded-2xl flex items-center justify-between px-12 py-3 shadow-lg text-lg z-50">
-      <div className="flex items-center gap-16">
-        <div className="h-20 w-32 bg-gray-500 rounded-md"></div>
-        <nav className="flex gap-8 text-black">
-          <Link href="#">
-            <HomeIcon className="stroke-black fill-none h-8 w-auto hover:scale-115 duration-500"/>
-          </Link>
+    <header className="fixed top-6 left-6 right-6 bg-white rounded-2xl flex items-center justify-between px-12 py-2 shadow-lg z-50">
+      <div className="flex items-center gap-8">
+        <Image src={logo} alt="Logo SoliFit" className="h-24 w-auto object-contain" />
+        <nav className="flex gap-8 text-black text-lg">
           <Link href="#" className="hover:underline underline-offset-2"> Qui sommes-nous ? </Link>
           <Link href="#" className="hover:underline underline-offset-2"> Fonctionnement </Link>
           <Link href="#" className="hover:underline underline-offset-2"> Témoignages </Link>
           <Link href="#" className="hover:underline underline-offset-2"> Engagement RSE et Partenaires </Link>
         </nav>
       </div>
-      <div className="flex gap-4 uppercase">
-        <Link href="/#" className="px-6 py-2 bg-gray-500 text-white border-2 border-gray-500 rounded-lg hover:bg-transparent hover:text-black duration-500"> Nous rejoindre </Link>
-        <Link href="/#" className="px-6 py-2 border-2 border-gray-500 rounded-lg hover:bg-gray-500 hover:text-white duration-500"> Votre espace </Link>
+      <div className="flex items-center gap-4 text-lg">
+        <Link href="/#" className="px-8 py-4 bg-[#BDF0DF] text-black rounded-full font-semibold duration-500"> Nous rejoindre </Link>
+        <Link href="/" className=" ">
+          <PersonIcon className=" fill-none stroke-black w-6 h-auto"/>
+        </Link>
       </div>
     </header>
   );
--- asso-sport/src/app/page.tsx
+++ asso-sport/src/app/page.tsx
@@ -1,25 +1,29 @@
 import Image from "next/image";
 import Link from "next/link";
 import image_runners from "@/assets/images/image_runners.jpg"
-import image_mockup from "@/assets/images/image_runners.jpg"
-import {CheckIcon, StarIcon} from "@/Components/Globals/icons";
+import image_mockup from "@/assets/images/image_mockup.png"
+import image_people_seat from "@/assets/images/image_people_seat.png"
+import image_notebook from "@/assets/images/image_notebook.png"
+import image_donation from "@/assets/images/image_donation.png"
+import image_hands from "@/assets/images/image_hands.png"
+import {CheckIcon, CurvedArrowIconLeft, CurvedArrowIconRight, StarIcon} from "@/Components/Globals/icons";
 
 export default function Home() {
   return (
-    <main className="h-screen w-full relative overflow-x-hidden">
-      <section id="home" className="pt-72 relative pb-16 flex justify-between gap-12 px-32">
-        <Image src={image_runners} alt="Photo de personne qui cours" className="absolute top-0 left-0 w-full h-3/4 object-cover"/>
-        <div className="absolute bg-black/40 top-0 left-0 w-full h-3/4"></div>
+    <main className=" w-full relative overflow-hidden">
+      <section id="home" className="pt-56 2xl:pt-72 relative pb-16 flex justify-between gap-12 px-32">
+        <Image src={image_runners} alt="Photo de personne qui cours" className="absolute top-0 left-0 w-full h-5/6 object-cover"/>
+        <div className="absolute bg-black/40 top-0 left-0 w-full h-5/6"></div>
         <div className="relative text-white w-2/3 ">
           <div>
-            <p className="text-6xl pb-6">Transformez chaque kilomètre en un geste solidaire</p>
-            <p className="text-2xl pb-10">
-              Comme + de 1000 TPE-PME dans votre région.​ <br/>
+            <h1 className="text-5xl pb-6">Transformez chaque kilomètre en un geste solidaire</h1>
+            <p className="text-xl pb-10">
+              Comme + de 1000 TPE-PME dans votre région. <br/>
               Fidélisez, et motivez vos collaborateurs tout en soutenant des causes qui comptent pour votre entreprise et vos équipes !
             </p>
             <Link href="#" className=" px-8 py-4 rounded-full text-xl bg-[#BDF0DF] text-black">Nous rejoindre</Link>
           </div>
-          <div className="flex gap-6 mt-36">
+          <div className="flex gap-6 mt-36 2xl:mt-56">
             <div className="flex-1 h-32 bg-[#D9D9D9] rounded-2xl"></div>
             <div className="flex-1 h-32 bg-[#D9D9D9] rounded-2xl"></div>
             <div className="flex-1 h-32 bg-[#D9D9D9] rounded-2xl"></div>
@@ -27,13 +31,13 @@ export default function Home() {
           </div>
         </div>
         <div className="pt-24">
-          <div className=" relative min-w-[400px] aspect-[0.8] rounded-2xl bg-gradient-to-b from-[#BDF0DF] to-[#FEFFB0]">
+          <div className=" relative min-w-[370px] aspect-[0.8] rounded-2xl bg-gradient-to-b from-[#BDF0DF] to-[#FEFFB0]">
           </div>
         </div>
       </section>
 
       <section id="presentation" className="max-w-7xl mx-auto text-center pt-8 pb-20">
-        <h2 className="text-5xl mb-12">Qui sommes nous ?</h2>
+        <h2 className="text-4xl mb-12">Qui sommes nous ?</h2>
         <p className="text-xl">Nous sommes une initiative innovante qui allie sport et solidarité pour transformer chaque effort en un impact concret. Notre mission : mobiliser les entreprises et leurs collaborateurs autour de défis sportifs collectifs, où chaque kilomètre parcouru contribue à soutenir des associations engagées dans des causes essentielles telles que l’environnement, la santé, l’éducation, et l’inclusion.</p>
       </section>
 
@@ -60,15 +64,30 @@ export default function Home() {
       </section>
 
       <section id="about" className="max-w-7xl mx-auto py-20">
-        <h2 className="text-5xl mb-12 text-center">Comment ça marche</h2>
-        <div className="flex justify-center items-start gap-24">
-          <div className="shadow-xl aspect-square rounded-full flex flex-col p-12 justify-center">
-            <h3>Etape 3</h3>
+        <h2 className="text-4xl mb-12 text-center">Comment ça marche</h2>
+        <div className="flex justify-between items-start">
+          <div className="relative shadow-xl aspect-square rounded-full flex flex-col p-8 justify-center items-center text-center text-xl font-semibold gap-2 w-64">
+            <h3>Etape 1</h3>
+            <Image src={image_notebook} alt="personne assise image" className="h-14 w-auto object-contain" />
             <p>Inscrivez votre entreprise</p>
+            <CurvedArrowIconRight className="absolute top-[110%] left-1/2 fill-[#2D8BBA] w-44 h-auto"/>
           </div>
-          <div className="shadow-xl aspect-square rounded-full flex flex-col p-12 justify-center mt-40 ">
+          <div className=" relative shadow-xl aspect-square rounded-full flex flex-col p-8 justify-center items-center text-center mt-40 text-xl font-semibold gap-2 w-64">
+            <h3>Etape 2</h3>
+            <Image src={image_people_seat} alt="personne assise image" className="h-14 w-auto object-contain" />
+            <p>Mobilisez vos équipes</p>
+            <CurvedArrowIconLeft className="absolute bottom-[110%] left-1/2 fill-[#2D8BBA] w-44 h-auto"/>
+          </div>
+          <div className=" relative shadow-xl aspect-square rounded-full flex flex-col p-8 justify-center items-center text-center text-xl font-semibold gap-2 w-64">
             <h3>Etape 3</h3>
-            <p>Inscrivez votre entreprise</p>
+            <Image src={image_hands} alt="personne assise image" className="h-14 w-auto object-contain" />
+            <p>Marchez, courez, dépassez-vous</p>
+            <CurvedArrowIconRight className="absolute top-[105%] left-1/2 fill-[#2D8BBA] w-44 h-auto"/>
+          </div>
+          <div className="shadow-xl aspect-square rounded-full flex flex-col p-8 justify-center items-center text-center mt-40 text-xl font-semibold gap-2 w-64">
+            <h3>Etape 4</h3>
+            <Image src={image_donation} alt="personne assise image" className="h-14 w-auto object-contain" />
+            <p>Mobilisez vos équipes</p>
           </div>
         </div>
       </section>
@@ -76,7 +95,7 @@ export default function Home() {
       <section id="application" className="bg-gradient-to-r from-[#FCD8EE] to-[#FEFFB0] py-20">
         <div className="max-w-7xl mx-auto flex justify-between gap-20 ">
           <div className="w-1/2">
-            <h2 className="text-5xl mb-12">L'application</h2>
+            <h2 className="text-4xl mb-12">L'application</h2>
             <ul className="space-y-4">
               <li className='flex gap-3 items-center bg-white/40 px-4 w-full py-2 rounded-full'>
                 <CheckIcon className="w-8 h-auto stroke-black fill-none"/>
@@ -100,52 +119,52 @@ export default function Home() {
               </li>
             </ul>
           </div>
-          <Image src={image_mockup} alt="image mockup application" className=" -mb-20 w-full aspect-square" />
+          <Image src={image_mockup} alt="image mockup application" className=" -mt-16 -mb-48 object-contain aspect-square w-1/2" />
         </div>
-
-
       </section>
 
-      <section id="testimonies" className="w-full bg-blue-default max-width-7xl mx-auto px-5 py-20 flex-wrap">
-        <div className="flex-col items-center text-center w-full text-white pb-16">  
-          <h2 className="m-2 text-2xl"><strong>Reconnue</strong> par les entreprises</h2>
-          <h2 className="m-2 text-2xl"><strong>Adorée</strong> par les entreprises</h2>
-        </div>
+      <section id="testimonies" className="w-full bg-blue-default py-20">
+        <div className="maw-w-7xl mx-auto px-4">
+          <div className="flex-col items-center text-center w-full text-white pb-16">
+            <h2 className="m-2 text-2xl"><strong>Reconnue</strong> par les entreprises</h2>
+            <h2 className="m-2 text-2xl"><strong>Adorée</strong> par les entreprises</h2>
+          </div>
 
-        <div className="flex justify-between items-stretch">
-          <article className="flex-col items-center justify-center w-1/4 mx-auto bg-white p-5 rounded-2xl text-center">
-            <div className="flex gap-2 justify-center m-3">
-              <StarIcon className="fill-star-default"/>
-              <StarIcon className="fill-star-default"/>
-              <StarIcon className="fill-star-default"/>
-              <StarIcon className="fill-star-default"/>
-              <StarIcon className="fill-star-default"/>
-            </div>
-            <p className="m-2">En tant qu'entreprise, nous avions du mal à motiver nos équipes avec les outils traditionnels. Depuis que nous utilisons cette solution, nos collaborateurs bénéficient d'activités engageantes et d'un suivi clair de leurs performances. Côté entreprise, la gestion est simplifiée grâce à une plateforme ergonomique qui regroupe tout en un seul endroit."</p>
-            <p><strong>— Marie, Responsable RH</strong></p>
-          </article>
-          <article className="flex-col items-center justify-center w-1/4 mx-auto bg-white p-5 rounded-2xl text-center">
-            <div className="flex gap-2 justify-center m-3">
-              <StarIcon className="fill-star-default"/>
-              <StarIcon className="fill-star-default"/>
-              <StarIcon className="fill-star-default"/>
-              <StarIcon className="fill-star-default"/>
-              <StarIcon className="fill-star-default"/>
-            </div>
-            <p className="m-2">"Nous cherchions une solution moderne pour dynamiser nos équipes tout en restant alignés avec nos valeurs d’entreprise. Cette application a complètement transformé notre approche. En plus de motiver les salariés avec des défis sportifs personnalisés, nous pouvons communiquer facilement nos engagements RSE et impliquer les collaborateurs dans des causes qui leur tiennent à cœur."</p>
-            <p><strong>— Antoine, Responsable RSE</strong></p>
-          </article>
-          <article className="flex-col items-center justify-center w-1/4 mx-auto bg-white p-5 rounded-2xl text-center">
-            <div className="flex gap-2 justify-center m-3">
-              <StarIcon className="fill-star-default"/>
-              <StarIcon className="fill-star-default"/>
-              <StarIcon className="fill-star-default"/>
-              <StarIcon className="fill-star-default"/>
-              <StarIcon className="fill-star-default"/>
-            </div>
-            <p className="m-2">"Nous avions déjà un programme d'avantages, mais il manquait une vraie personnalisation. Avec cette solution, nous avons pu offrir des activités sur mesure à nos collaborateurs et centraliser tous les services dans une application intuitive. Le gain de temps pour les administrateurs est énorme, et les salariés sont ravis de voir des récompenses concrètes pour leurs efforts."</p>
-            <p><strong>— Camille, Élue CSE</strong></p>
-          </article>
+          <div className="flex justify-between items-stretch flex-wrap">
+            <article className="flex-col items-center justify-center w-1/4 mx-auto bg-white p-5 rounded-2xl text-center">
+              <div className="flex gap-2 justify-center m-3">
+                <StarIcon className="fill-star-default"/>
+                <StarIcon className="fill-star-default"/>
+                <StarIcon className="fill-star-default"/>
+                <StarIcon className="fill-star-default"/>
+                <StarIcon className="fill-star-default"/>
+              </div>
+              <p className="m-2">En tant qu'entreprise, nous avions du mal à motiver nos équipes avec les outils traditionnels. Depuis que nous utilisons cette solution, nos collaborateurs bénéficient d'activités engageantes et d'un suivi clair de leurs performances. Côté entreprise, la gestion est simplifiée grâce à une plateforme ergonomique qui regroupe tout en un seul endroit."</p>
+              <p><strong>— Marie, Responsable RH</strong></p>
+            </article>
+            <article className="flex-col items-center justify-center w-1/4 mx-auto bg-white p-5 rounded-2xl text-center">
+              <div className="flex gap-2 justify-center m-3">
+                <StarIcon className="fill-star-default"/>
+                <StarIcon className="fill-star-default"/>
+                <StarIcon className="fill-star-default"/>
+                <StarIcon className="fill-star-default"/>
+                <StarIcon className="fill-star-default"/>
+              </div>
+              <p className="m-2">"Nous cherchions une solution moderne pour dynamiser nos équipes tout en restant alignés avec nos valeurs d’entreprise. Cette application a complètement transformé notre approche. En plus de motiver les salariés avec des défis sportifs personnalisés, nous pouvons communiquer facilement nos engagements RSE et impliquer les collaborateurs dans des causes qui leur tiennent à cœur."</p>
+              <p><strong>— Antoine, Responsable RSE</strong></p>
+            </article>
+            <article className="flex-col items-center justify-center w-1/4 mx-auto bg-white p-5 rounded-2xl text-center">
+              <div className="flex gap-2 justify-center m-3">
+                <StarIcon className="fill-star-default"/>
+                <StarIcon className="fill-star-default"/>
+                <StarIcon className="fill-star-default"/>
+                <StarIcon className="fill-star-default"/>
+                <StarIcon className="fill-star-default"/>
+              </div>
+              <p className="m-2">"Nous avions déjà un programme d'avantages, mais il manquait une vraie personnalisation. Avec cette solution, nous avons pu offrir des activités sur mesure à nos collaborateurs et centraliser tous les services dans une application intuitive. Le gain de temps pour les administrateurs est énorme, et les salariés sont ravis de voir des récompenses concrètes pour leurs efforts."</p>
+              <p><strong>— Camille, Élue CSE</strong></p>
+            </article>
+          </div>
         </div>
       </section>
 
